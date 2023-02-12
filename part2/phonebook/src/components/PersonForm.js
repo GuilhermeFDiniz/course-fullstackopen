2a51343e79ddfbdb50855bb13c63d22b19121b48
Guilherme Diniz <guidinizeng@gmail.com>
2023-02-12 13:21:20 -0300

    Fix Part 2 Exercise 2.10

--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -3,7 +3,6 @@ import React from 'react'
 function PersonForm({handleForm, handleInputName, handleInputNumber, newName, newNumber }) {
   return (
     <form onSubmit={handleForm}>
-      <h2>add a new</h2>
         <div>
           name: <input value={newName} onChange={handleInputName}/>
           <br></br>
