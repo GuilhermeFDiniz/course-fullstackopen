224f87ac024db300cd73ab33320c9070af230020
Guilherme Ferreira Diniz <guilhermediniz@MacBookPro.local>
2024-05-13 09:09:20 -0300

    implementing exercises

--- part3/index.js
+++ part3/index.js
@@ -1,53 +1,110 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+
+let persons = [
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
   },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
   },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
   }
 ]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World</h1>')
-})
-app.get('/api/notes', (request, response) => {
-  response.send(notes)
+//#3.1
+app.get('/api/persons', (request, response) => {
+  response.send(persons)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+
+//#3.2
+app.get('/api/info', (request, response) => {
+  response.send(`<p>Phone has info for ${persons.length} people</p><p>${new Date()}</p>`)
+})
+
+//#3.3
+app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if(note){
-
-    response.json(note)
+  const person = persons.find(person => person.id === id)
+  if(person){
+    response.send(person)
   } else {
-
     response.status(404).end()
   }
 })
 
-app.delete('/api/notes/:id', (request, response)=> {
-const id = Number(request.params.id)
-notes = notes.filter(note => note.id !== id)
-console.log(notes)
-response.status(204).end()
+//#3.4
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
+//#3.5 and #3.6
+const generateId = () => {
+  return Math.random( 1, 999999 )
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const checkIfNameExists = persons.find(person => person.name === body.name)
+
+  if (checkIfNameExists) {
+    return response.status(400).json({
+      error: 'name must be unique'
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+  response.json(person)
+})
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
-
   console.log(`server running on port ${PORT}`)
 })
